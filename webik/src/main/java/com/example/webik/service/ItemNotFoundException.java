c5362a388f35e3f4fcf2d9bff7e7c91facfa1944
AzizyanAsi <81428301+AzizyanAsi@users.noreply.github.com>
2021-10-03 07:26:38 -0700

    Delete ItemNotFoundException.java

--- webik/src/main/java/com/example/webik/service/ItemNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.example.webik.service;
-
-public class ItemNotFoundException extends Throwable {
-    @Override
-    public String toString() {
-        return "item is not found";
-    }
-}
