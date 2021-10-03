0f380029f104a74ad5b08bb13d4baa05bfb608f9
AzizyanAsi <81428301+AzizyanAsi@users.noreply.github.com>
2021-10-03 07:29:06 -0700

    Delete src directory

--- src/main/java/com/example/webik/config/myConfig.java
+++
@@ -1,9 +0,0 @@
-package com.example.webik.config;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan(value = "com.example")
-public class myConfig {
-}
